3ebd6e01aca1a07f8ed1b5a9b519524529685a6b
Thai Phan <thaiphan90@yahoo.com>
2019-10-12 20:41:42 -0500

    google api

--- server.js
+++ server.js
@@ -2,11 +2,25 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const axios = require('axios');
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Connects to google API to retrieve book information
+axios.get("https://www.googleapis.com/books/v1/volumes?q=1984")
+.then((response) => {
+    console.log("response", response.data)
+})
+.catch((error) => {
+    console.log("error", error);
+});
 
 // Send every request to the React app
 // Define any API routes before this runs
