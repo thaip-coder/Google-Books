02f29a9f513041e689b253339755f117e9fe3f0b
Thai Phan <thaiphan90@yahoo.com>
2019-10-12 20:57:20 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+const express = require('express');
+const path = require('path');
 const app = express();
 const axios = require('axios');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -11,22 +14,18 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+};
+
+app.use(routes);
+
+mongoose.connect("mongodb://localhost/populate", { useNewUrlParser: true });
+
+
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
-
-// Connects to google API to retrieve book information
-axios.get("https://www.googleapis.com/books/v1/volumes?q=1984")
-.then((response) => {
-    console.log("response", response.data)
-})
-.catch((error) => {
-    console.log("error", error);
-});
-
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
