a50757a61da7fe1aa93f48546a410243bcc881cb
Thai Phan <thaiphan90@yahoo.com>
2019-10-14 14:59:00 -0500

    book schema exported

--- models/book.js
+++ models/book.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    author: { type: String, required: true};
+    author: { type: String, required: true},
     description: String,
     image: String,
     link: String,
