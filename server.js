265df8d16a39edb81f7d7235ed334f10d5c92f80
Thai Phan <thaiphan90@yahoo.com>
2019-10-14 14:58:03 -0500

    created book schema

--- server.js
+++ server.js
@@ -31,39 +31,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Google Book Search
-app.get("/api/books/:book", (req, res) => {
-  axios.get("https://www.googleapis.com/books/v1/volumes?q=" + req.params.book)
-    .then((response) => {
-      console.log("response", response.data)
-      res.json(response.data);
-    })
-    .catch((error) => {
-      console.log("error", error);
-    })
-})
 
-// Saves Book to MongoDB - Not Working Yet
-app.post("/api/submit", (req, res) => {
-  db.Book.create(req.body)
-    .then(dbBook => {
-      res.json(dbBook);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-})
-
-// View All Saved Books - Not Working Yet
-app.get("/api/saved", (req, res) => {
-  db.Book.find({})
-    .then(dbArticle => {
-      res.json(dbArticle);
-    })
-    .catch(err => {
-      res.json(err);
-    })
-})
 
 // Send every request to the React app - Define any API routes before this runs
 app.get("*", function (req, res) {
