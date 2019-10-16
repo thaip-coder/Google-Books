253e3740ca05d6a58e8d7ef5aed05cf144e1e0b4
Thai Phan <thaiphan90@yahoo.com>
2019-10-16 14:09:30 -0500

    establishing pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
+import Home from "./Home";
+import Nav from "./Nav";
+import Saved from "./Saved";
+ 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>React Is Working</h1>
+        <Nav />
+        <Home />
+        <Saved />
       </div>
     );
   }
---
+++ client/src/Nav.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class Nav extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Nav bar</h1>
+      </div>
+    );
+  }
+}
+
+export default Nav;
