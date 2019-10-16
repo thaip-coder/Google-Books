bf4362b6e495442012341ebd504f3429fdab19ff
Thai Phan <thaiphan90@yahoo.com>
2019-10-16 15:28:44 -0500

    working home and saved links

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 const Nav = () => {
@@ -7,8 +8,12 @@ const Nav = () => {
       <nav>
         <ul className="navbar-nav bg-light">
             <li><h1>Google Book Search</h1></li>
+            <Link to='/'>
             <li>Home</li>
+            </Link>
+            <Link to="/Saved">
             <li>Saved Books</li>
+            </Link>
         </ul>
       </nav>
     );
